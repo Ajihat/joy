b51f4898709fc9a158aaeb4bfb443d48a970a481
Dawid Lechowski <david.lechowski@gmail.com>
2023-01-17 11:21:47 +0100

    add babycare mainSliderData

---
+++ src/data/Babycare/mainSliderData.ts
@@ -0,0 +1,31 @@
+import img1 from 'assets/babycare/babycare_mainSlider_img1.jpg';
+import img2 from 'assets/babycare/babycare_mainSlider_img2.jpg';
+import img3 from 'assets/babycare/babycare_mainSlider_img3.jpg';
+import img4 from 'assets/babycare/babycare_mainSlider_img4.jpg';
+
+export const mainSliderData = [
+	{
+		id: 1,
+		image: img1,
+		text: 'Zamiłowanie do muzyki',
+		href: 'ada',
+	},
+	{
+		id: 2,
+		image: img2,
+		text: 'Natura i ruch',
+		href: 'ada',
+	},
+	{
+		id: 3,
+		image: img3,
+		text: 'Bliskie więzi',
+		href: 'ada',
+	},
+	{
+		id: 4,
+		image: img4,
+		text: 'Swoboda tworzenia',
+		href: 'ada',
+	},
+];
