132208bea9e190f71ff28924b2d4791e3d7ec520
Dawid Lechowski <david.lechowski@gmail.com>
2023-02-13 12:26:32 +0100

    social logo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -26,10 +26,7 @@ export const Home = () => {
 					content='Witamy w Joy Preschool & Babycare. To przyjazne miejsce dla Twojego dziecka.'
 				/>
 				<meta property='og:type' content='website' />
-				<meta
-					property='og:image'
-					content='https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png'
-				/>
+				<meta property='og:image' content='social-logo.jpg' />
 				<meta property='og:image:width' content='390' />
 				<meta property='og:image:height' content='215' />
 			</Helmet>
