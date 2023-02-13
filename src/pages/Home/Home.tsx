01e4c7a29e54bb3a90c71a4d2b147afeb8602f32
Dawid Lechowski <david.lechowski@gmail.com>
2023-02-13 11:49:56 +0100

    social logo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -25,6 +25,12 @@ export const Home = () => {
 					property='og:description'
 					content='Witamy w Joy Preschool & Babycare. To przyjazne miejsce dla Twojego dziecka.'
 				/>
+				<meta property='og:url' content={`${appRoutes.homePage}`} />
+				<meta property='og:type' content='website' />
+				<meta
+					property='og:image'
+					content='https://raw.githubusercontent.com/Ajihat/joy/main/src/assets/social-logo.jpg'
+				/>
 			</Helmet>
 			<div className={styles.logoHolder}>
 				<HomeLogo />
